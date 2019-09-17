4dfa69c1d9710a940072eea97c9a0ffef8bdeb1c
Arpan Das <arp14@yahoo.com>
2019-09-17 15:18:38 +0530

    Main updated

--- src/com/w3epic/fileencryptionhelper/Main.java
+++ src/com/w3epic/fileencryptionhelper/Main.java
@@ -11,16 +11,33 @@ public class Main {
 //		String fileToBeEncryptedPath = "./69449593_2414227948816523_124365601303953408_o.png.jpg";
 //		String encryptedFilePath = "./encryptedFile";
 //		
-//		encryptor.encryptFile(receiverPublicKeyFilePath, fileToBeEncryptedPath, encryptedFilePath);
+//		String[] parts = fileToBeEncryptedPath.split("\\.");
+//		String extension = parts[parts.length - 1];
+//		encryptedFilePath = fileToBeEncryptedPath.replace("." + extension, "") + "_encrypted." + extension;
+//		
+//		if (encryptor.encryptFile(receiverPublicKeyFilePath, fileToBeEncryptedPath, encryptedFilePath))
+//			System.out.println("Encrypted successfully to: " + encryptedFilePath);
+//		else
+//			System.out.println("Encryption failed!");
+		
+		
 		
 		
 		// decrypt
 		String privateKeyFilePath = "./receiver_keys/private.der";
-		String encryptedFilePath2 = "./encryptedFile";
+		String encryptedFilePath2 = "f7d3885e-0089-466c-8ed4-f9408f6d589f.jpg";//"./encryptedFile";
 		String decryptedFilePath = "./decryptedFile.jpg";
 		
+		String[] parts = encryptedFilePath2.split("\\.");
+		String extension = parts[parts.length - 1];
+		decryptedFilePath = encryptedFilePath2.replace("." + extension, "") + "_decrypted." + extension;
+		
 		FileEncryptionHelper encryptor2 = new FileEncryptionHelper();
-		encryptor2.decryptFile(privateKeyFilePath, encryptedFilePath2, decryptedFilePath);
+		
+		if (encryptor2.decryptFile(privateKeyFilePath, encryptedFilePath2, decryptedFilePath))
+			System.out.println("Decrypted successfully to: " + decryptedFilePath);
+		else
+			System.out.println("Decryption failed!");
 		
 	}
 }
